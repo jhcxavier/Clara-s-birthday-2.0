8c82763f3e324dc0f9afce16476348f354d7aa71
João Henrique Xavier <jhcxavier@gmail.com>
2020-07-17 11:44:09 -0400

    updates

--- src/component/jumboIntro.js
+++ src/component/jumboIntro.js
@@ -6,28 +6,28 @@ const JumboIntro = () => {
       <div className="container">
         <h1 className="display-4">Desafios </h1>
         <p className="lead">
-          Esta e uma brincadeira que envolve a Clara, o papai e a mamae. Serao 3
-          desafios, para cada um a Clara recebera um presente.
+          Está e uma brincadeira que envolve a Clara, o papai e a mamãe. Serão 3
+          desafios, para cada um a Clara ganhará um presente.
           <br /> <strong>O primeiro desafio:</strong>
-          <br /> - Primeira parte sera resolvida pela mamae da Clara, nessa
-          etapa a Clara pode ajudar a mamae. Ali existe a senha para desbloquear
+          <br /> - Primeira parte será resolvida pela Mariana, nessa
+          etapa a Clara pode ajudar a mamãe. Ali existe a senha para desbloquear
           o segundo desafio.
-          <br /> - A segunda parte sera apenas para a Clara, sem ajuda da Mamae.{" "}
+          <br /> - A segunda parte será apenas para a Clara, sem ajuda da Mamãe.{" "}
           <br />
           <strong>O segundo desafio:</strong>
-          <br /> - A primeira etapa do segundo desafio, devera ser solucionado
+          <br /> - A primeira etapa do segundo desafio, deverá ser solucionado
           pelo papai da Clara. Ele vai encontrar uma parte da senha que
-          desbloqueara o terceiro desafio.
-          <br /> - A segunda etapa, novamente sera resolvida pela Clara sozinha.
-          O terceiro desafio: Esse sera resolvido pela Clara{" "}
+          desbloqueará o terceiro desafio.
+          <br /> - A segunda etapa, novamente deverá ser resolvida pela Clara sem ajuda.
+          O terceiro desafio: Esse será resolvido pela Clara{" "}
           <strong>sem ajuda de ninguem</strong>
           <br />
           <strong>Terceiro desafio:</strong>
           <br />
-          So para Clara!
+          Segredo!
         </p>
         <h6>
-          <strong>Obs:</strong> Tudo funciona perfeitamente. Nao adianta me
+          <strong>Obs:</strong> Tudo funciona perfeitamente. Não adianta me
           ligar pra pedir ajuda, dica, nada. Me esquece!
         </h6>
       </div>
